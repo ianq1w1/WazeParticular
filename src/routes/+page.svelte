24b8ded4a7c1e4b2845b448ed53ef900335a52a6
ianq1w1 <ianpereiraian2@gmail.com>
2023-08-30 19:17:38 -0300

    paginação, testes básicos

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>WAZE PARTICULAAAAAAAAR (essa aqui é a home)</h1>
+<a href="/cidade">clique aqui para começar (isso era pra ser um link)</a>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/cidade/+page.svelte
@@ -0,0 +1,3 @@
+<h1>em tese mapa do jogo</h1>
+<a href="/">link pra voltar á home</a>
+<h2>aqui em baixo array :V</h2>
