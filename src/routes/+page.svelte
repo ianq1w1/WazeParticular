e9f93bbe37edc7d658295d3e4fb9d40063a0961e
CarolayneMR <carolwolf0101@gmail.com>
2023-09-06 21:12:09 -0300

    E o pix, nada ainda?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,64 @@
-<h1>WAZE PARTICULAAAAAAAAR (essa aqui é a home)</h1>
-<a href="/cidade">clique aqui para começar (isso era pra ser um link)</a>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>WAZE</title>
+<body>
+    <main>
+        <div class="mainBox">
+            <h1 class="title">WAZE PARTICULAR</h1>
+            <div class="menu">
+                <a href="/cidade">
+                    <button class="btn">Começar</button>
+                </a>
+                <br>
+                <a href="/">
+                    <button class="btn">Criar</button>
+                </a>
+                <br>
+                <a href="/">
+                    <button class="btn">Sobre</button>
+                </a>  
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        Copyright © Kerou & Iain
+    </footer>
+</body>
+
+
+<style>
+    body{
+        
+        background: #cccccc;
+        text-align: center;
+        font-size: 40px;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    footer{
+        display: flex;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    .btn {
+        background-color: #4CAF50;
+        width: 30%;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 25px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+
+    .btn:hover {
+        background-color: #378f3a;
+    }
+</style>
